ad5a5e39074c63b516f65c217e35e17e67558b0e
Archer Scherer <ascherer831@gmail.com>
2021-05-19 11:56:27 -0500

    dojo_survey revisited complete.. I think

--- survey_form/urls.py
+++ survey_form/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.index),
+    path('process', views.process),
     path('result',views.result),
     
 ]
--- survey_form/views.py
+++ survey_form/views.py
@@ -1,17 +1,24 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def process(request):
+    request.session['name']= request.POST['name']
+    request.session['dojo']= request.POST['dojo']
+    request.session['lang']= request.POST['language']
+    request.session['comments']= request.POST['comments']
+    return redirect('/result')
     
 
 def result(request):
-    context = {
-        "name" :request.POST["name"],
-        "dojo" : request.POST["dojo"],
-        "lang" :request.POST["language"],
-        "comments" : request.POST["comments"],
-    }
-    return render(request, 'submissions.html', context)
+    # context = {
+    #     "name" :request.POST["name"],
+    #     "dojo" : request.POST["dojo"],
+    #     "lang" :request.POST["language"],
+    #     "comments" : request.POST["comments"],
+    # }
+    return render(request, 'submissions.html')
 
